14bff4efb05283687e19dfaced4444f9a52dc299
ArhamAliKamal <arhamali271221@gmail.com>
2025-02-18 03:06:23 +0500

    Baitussalam

--- 4_JavaScript/Assignments/Post-Class Assignments/array-largestnum.js
+++ 4_JavaScript/Assignments/Post-Class Assignments/array-largestnum.js
@@ -1,16 +1,16 @@
-function findLargestNumber(arr) {
-    if (arr.length === 0) {
+function findLargestNumber(obj) {
+    if (obj.length === 0) {
         return null;
     }
-    let largest = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > largest) {
-            largest = arr[i];
+    let largest = obj[0];
+    for (let i = 1; i < obj.length; i++) {
+        if (obj[i] > largest) {
+            largest = obj[i];
         }
     }
     return largest;
 }
 
-const numbers = [3, 5, 7, 2, 8, -1, 4, 10, 12];
+const numbers = {a:3, b:5, c:7, d:2, e:8, f:-1, g:4, h:10, i:12};
 const largestNumber = findLargestNumber(numbers);
 console.log("The largest number is:", largestNumber);
