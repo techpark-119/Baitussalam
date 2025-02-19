2d3c2d008404816587049848de4540edcabd9633
ArhamAliKamal <arhamali271221@gmail.com>
2025-02-19 07:51:38 +0500

    Baitussalam Post-Class Assignments

--- 4_JavaScript/Assignments/Post-Class Assignments/ToDoapp.js
+++ 4_JavaScript/Assignments/Post-Class Assignments/ToDoapp.js
@@ -1,19 +1,27 @@
 var ToDo = [];
-task = prompt("Welcome to your To-Do List App! Type Add to Add a task, Remove to remove a task, List to list all the tasks, and Quit to quit the app.");
+console.log("Welcome to your To-Do List App! Type Add to Add a task, Remove to remove a task, List to list all the tasks, and Quit to quit the app.");
+do  {
+    var prompt1 = prompt("What would you like to do?");
 
-if(prompt === "Add") {
-    ToDo.push(task);
-}
-else if(prompt === "Remove") {
-    var remove = prompt("Which task would you like to remove?");
-    var index = ToDo.indexOf(remove);
-    ToDo.splice(index, 1);
-}
-else if (prompt === "List") {
-    for (var i = 0; i < ToDo.length; i++) {
-        console.log(ToDo[i]);
+    if(prompt1 === "Add") {
+     var task = prompt("What task would you like to add?");
+     ToDo.push(task);
     }
-}
-else if (prompt === "Quit") {
+    else if(prompt1 === "Remove") {
+     var remove = prompt("Which task would you like to remove?");
+     var index = ToDo.indexOf(remove);
+     ToDo.splice(index, 1);
+    }
+    else if (prompt1 === "List") {
+     for (var i = 0; i < ToDo.length; i++) {
+     console.log(ToDo[i]);
+     }
+    }
+    else if (prompt1 === "Quit") {
     console.log("Goodbye!");
-}
+    break
+    }
+    else {
+    console.log("Invalid input. Please try again.");
+    }
+} while (prompt1 !== "Quit");
