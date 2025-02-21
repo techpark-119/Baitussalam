6717ac39fbefec1c54e29c60f0bb9394095d4c99
ArhamAliKamal <arhamali271221@gmail.com>
2025-02-22 02:40:13 +0500

    Baitussalam

--- 4.JavaScript/Assignments/Post-Class Assignments/Assignment-2b.js
+++ 4.JavaScript/Assignments/Post-Class Assignments/Assignment-2b.js
@@ -1,3 +1,20 @@
+// function findLargestNumber(obj) {
+//     if (obj.length === 0) {
+//         return null;
+//     }
+//     let largest = obj[0];
+//     for (let i = 1; i < obj.length; i++) {
+//         if (obj[i] > largest) {
+//             largest = obj[i];
+//         }
+//     }
+//     return largest;
+// }
+
+// const numbers = {a:3, b:5, c:7, d:2, e:8, f:-1, g:4, h:10, i:12};
+// const largestNumber = findLargestNumber(numbers);
+// console.log("The largest number is:", largestNumber);
+
 var ToDo = [];
 console.log("Welcome to your To-Do List App! Type Add to Add a task, Remove to remove a task, List to list all the tasks, and Quit to quit the app.");
 do  {
