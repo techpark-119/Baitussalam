fec178882ac2010419020d03b8725def1c917927
ArhamAliKamal <arhamali271221@gmail.com>
2025-03-03 08:38:03 +0500

    Add new ToDo app assignment with HTML, CSS, and JavaScript; update existing files and fix script references

---
+++ 4.JavaScript/1.Assignments/In-Class Assignments/InClass-1b.js
@@ -0,0 +1,18 @@
+// Code snippet to check user status
+let LoggedIn = true, ProSubscription = false;
+
+// This is the traditional and the most memory efficient way method.
+
+if (LoggedIn) {
+  if (ProSubscription) {
+    console.log(`Thanks, you are a Pro subscriber`);
+  } else {
+    console.log(`Here is your data, consider subscribing to Pro to gain access to premium features`);
+  }
+} else {
+  console.log(`Please login to see the data`);
+}
+
+// This is the ternary operator method, and it is less memory efficient than the if-else method.
+
+LoggedIn?ProSubscription?console.log(`Thanks, you are a Pro subscriber`):console.log(`Here is your data, consider subscribing to Pro to gain access to premium features`):console.log(`Please login to see the data`);
