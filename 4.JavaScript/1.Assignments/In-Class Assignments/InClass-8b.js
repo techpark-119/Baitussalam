7e596c77a1069e8921808dc5defd53aec36d7e59
techpark-119 <techpark-119@gmail.com>
2025-03-05 11:03:48 +0500

    Refactor pizza ordering code and add audio file

--- 4.JavaScript/1.Assignments/In-Class Assignments/InClass-8b.js
+++ 4.JavaScript/1.Assignments/In-Class Assignments/InClass-8b.js
@@ -1,14 +1,39 @@
 // Fixed this code using callback
 
-let data;
+// let data;
 
-function fetchData() {
+// function fetchData() {
+//     setTimeout(() => {
+//         data = { name: "John", age: 30 },2000)
+// }
+
+// console.log(data)
+
+// fetchData()
+
+// console.log("Data is being fetched...")
+
+function orderPizza(preparingPizza
+    , served) {
     setTimeout(() => {
-        data = { name: "John", age: 30 },2000)
+        console.log('Step 1, ordered')
+        preparingPizza(served)
+    }, 4000)
 }
 
-console.log(data)
+function preparingPizza(served) {
+    setTimeout(() => {
+        console.log('step 2, prepared')
+        served()
+    }, 3000)
+}
 
-fetchData()
+function served() {
+    setTimout(() => {
+        console.log('step 3, served')
+    }, 1000)
+}
 
-console.log("Data is being fetched...")
+orderPizza(preparingPizza,served)
+// preparingPizza()
+// served()
