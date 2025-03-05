2fbdfb90b1cfc76c1431230ac3174c078aa799f6
techpark-119 <techpark-119@gmail.com>
2025-03-05 11:47:09 +0500

    Refactor pizza ordering functions to use Promises and async/await

--- 4.JavaScript/1.Assignments/In-Class Assignments/InClass-8b.js
+++ 4.JavaScript/1.Assignments/In-Class Assignments/InClass-8b.js
@@ -13,15 +13,40 @@
 
 // console.log("Data is being fetched...")
 
-function orderPizza(preparingPizza
-    , served) {
-    setTimeout(() => {
-        console.log('Step 1, ordered')
-        preparingPizza(served)
-    }, 4000)
+// function orderPizza(preparingPizza
+//     , served) {
+//     setTimeout(() => {
+//         console.log('Step 1, ordered')
+//         preparingPizza(served)
+//     }, 4000)
+// }
+
+// function preparingPizza(served) {
+//     setTimeout(() => {
+//         console.log('step 2, prepared')
+//         served()
+//     }, 3000)
+// }
+
+// function served() {
+//     setTimout(() => {
+//         console.log('step 3, served')
+//     }, 1000)
+// }
+
+// orderPizza(preparingPizza,served)
+// preparingPizza()
+
+function orderPizza() {
+    const callback = (resolve, reject) => {
+        setTimeout(() => {
+            console.log('Step 1, ordered')
+            resolve()
+        }, 4000)
+    }
 }
 
-function preparingPizza(served) {
+function preparingPizza() {
     setTimeout(() => {
         console.log('step 2, prepared')
         served()
@@ -29,11 +54,30 @@ function preparingPizza(served) {
 }
 
 function served() {
-    setTimout(() => {
-        console.log('step 3, served')
-    }, 1000)
+    const callback =(resolve, reject) => {
+        setTimeout(() => {
+            console.log('step 3, served')
+        }, 1000)
+    }
+    return new Promise(callback);
 }
 
-orderPizza(preparingPizza,served)
-// preparingPizza()
-// served()
+// const orderPizzaPromise = orderPizza();
+// orderPizzaPromise.then((result) => {
+//     console.log(Result);
+//     const preparingPizzaPromise = preparingPizza();
+// preparingPizzaPromise.then((result) => {
+//     console.log(result);
+//     const servedPromise = served();
+//     servedPromise.then((result) => {
+//         console.log(result);
+//     })
+//}
+
+ async function main() {
+     const orderPizzaPromise = await orderPizza()
+     console.log(orderPizzaMessage)
+     const prepareP
+ }
+main()
+
